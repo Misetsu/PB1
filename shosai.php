92de6e12b7f9aea98a931c0fb06299f913218a91
Misetsu <nakitamak@gmail.com>
2024-04-18 14:37:30 +0900

    tag

--- shosai.php
+++ shosai.php
@@ -6,6 +6,20 @@ require_once __DIR__ . '/class.php';
 $form = new form();
 $all = $form->getQues($quesID);
 $allAns = $form->getAllAns($quesID);
+
+$options = array(
+    'option1' => 'C言語',
+    'option2' => 'C#',
+    'option3' => 'C++',
+    'option4' => 'Java',
+    'option5' => 'Python',
+    'option6' => 'HTML',
+    'option7' => 'JavaScript',
+    'option8' => 'SQL',
+    'option9' => 'CSS',
+    'option10' => 'PHP',
+    'option11' => 'その他',
+);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -19,10 +33,10 @@ $allAns = $form->getAllAns($quesID);
 
 
 <style>
-        .center {
-            text-align: center;
-        }
-    </style>
+    .center {
+        text-align: center;
+    }
+</style>
 
 <body>
 
@@ -33,6 +47,7 @@ $allAns = $form->getAllAns($quesID);
             echo '<h2>' . $row['title'] . '<h2>';
             echo '<h4>' . $row['name'] . '<h4>';
             echo '<p>' . $row['message'] . '<p>';
+            echo '<p class="tag">' . $options[$row['selection']] . '</p>';
         }
         ?>
     </div>
@@ -95,7 +110,7 @@ $allAns = $form->getAllAns($quesID);
 </body>
 
 <div class="center">
-        <a href="index.php">記事一覧を見る</a>
-    </div>
+    <a href="index.php">記事一覧を見る</a>
+</div>
 
 </html>
