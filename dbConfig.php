2e7c654dd57059b66140ebc4e581a26cc5dcb21c
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-04-18 09:50:31 +0900

    hennkou

--- dbConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define("DB_SERVER", "localhost");  // サーバー
-    define("DB_USER", "root");         // ユーザー
-    define("DB_PASS", "");             // パスワード
-    define("DB_NAME", "ilove");      // データベース
-?>
