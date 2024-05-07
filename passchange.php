537517a2c8bad3c478eaadc3dc4fe424b0f05a0b
Takaishi Yuto <kd1336678@st.kobedenshi.ac.jp>
2024-05-07 11:50:28 +0900

    A

---
+++ passchange.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>パスワード変更ページ</title>
+    <link rel="stylesheet" href="login.css" />
+</head>
+
+<body>
+    <header>
+        <button onclick="location.href='home.html'" style="font-size: 24px;">🏠</button>
+        <h1>パスワード変更ページ</h1>
+    </header>
+    <h1>パスワードを変更するために現在のパスワードと新しいパスワードを入力してください</h1>
+    <form action="login_db.php" method="post">
+        <div class="form-group">
+            <label for="password">現在のパスワード：</label>
+            <input type="password" id="password" name="password" required />
+        </div>
+        <div class="form-group">
+            <label for="new-password">新しいパスワード：</label>
+            <input type="new-password" id="new-password" name="new-password" required />
+        </div>
+        <button type="submit">変更</button>
+    </form>
+</body>
+
+</html>
